0aeacbd33ef7e91dcccbd3cb01e9f750ac76340b
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2020-06-02 17:50:10 +0300

    Adds methods

--- src/main/java/com/codecool/datastructure/SinglyLinkedList.java
+++ src/main/java/com/codecool/datastructure/SinglyLinkedList.java
@@ -1,6 +1,9 @@
 package com.codecool.datastructure;
 
+import java.util.List;
+
 public class SinglyLinkedList {
+
 
     private class Link {
 
@@ -27,22 +30,69 @@ public class SinglyLinkedList {
     private Link head;
 
     public SinglyLinkedList() {
+        this.head = null;
     }
 
     // Returns the number at 'index'.
     public int access(int index) {
-        return 0;
+        if (index == 0) {
+            return head.getValue();
+        }
+        Link current = head;
+        Link previous = null;
+        for (int i= 0; i <= index; i++){
+            previous = current;
+            current = current.getNext();
+            if(current==null){
+                break;
+            }
+        }
+        assert previous != null;
+        return previous.getValue();
     }
 
     // Returns the index of 'number' if it's in the array, otherwise -1;
     public int search(int number) {
-        return 0;
+        Link current = head;
+        int index = 0;
+        while(current.getNext() != null){
+            if(current.getValue() == number){
+                return index;
+            }
+            index++;
+            current = current.getNext();
+        }
+        return -1;
     }
 
     // Inserts 'number' at 'index' into the array shifting elements if necessary.
     //
     // e.g. the result of inserting 42 at index 3 into [0, 1, 2, 3, 4] is [0, 1, 2, 42, 3, 4]
     public void insert(int index, int number) {
+        Link valueToInsert = new Link(number);
+
+        if(head==null && index!=0){
+            return;}
+        else{
+            head=new Link(number);
+        }
+        if(index==0){
+            valueToInsert.setNext(head);
+            head=valueToInsert;
+            return;
+        }
+
+        Link current = head;
+        Link previous = null;
+        for (int i= 0; i < index; i++) {
+            previous = current;
+            current = current.getNext();
+            if (current == null){
+                break;
+            }
+        }
+        valueToInsert.setNext(current);
+        previous.setNext(valueToInsert);
     }
 
     // Deletes the element at 'index' from the array.
